03040573b6c7391cd949a9411f2216e3f1ad5b7a
Fayaz <terifeabdulla@gmail.com>
2023-02-15 07:38:39 +0400

    task

---
+++ ExtentionMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aww
+{
+    internal static class ExtentionMethods
+    {
+        public static bool IsContainsDigit(this string name)
+        {
+            if (name == null) return false;
+
+            foreach (var cr in name)
+            {
+                if (char.IsDigit(cr))
+                    return true;
+            }
+            return false;
+        }
+        public static string ToCapitalize(this string str)
+        {
+            string newStr = Char.ToUpper(str[0]) + str.Substring(1).ToLower();
+            return newStr;
+        }
+        public static bool IsOdd(this int num1)
+        {
+            if (num1 % 2 != 0) return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsEven(this int num2)
+        {
+            if (num2 % 2 == 0)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aww
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tek eded yazin:");
+            string numStrOdd = Console.ReadLine();
+            int numOdd = Convert.ToInt32(numStrOdd);
+            Console.WriteLine(numOdd.IsOdd());
+
+            Console.WriteLine("Cut eded yazin:");
+            string numStrEven = Console.ReadLine();
+            int numEven = Convert.ToInt32(numStrEven);
+            Console.WriteLine(numEven.IsEven());
+
+            Console.WriteLine("Yazi yazin:");
+            string str = Console.ReadLine();
+            Console.WriteLine(str.IsContainsDigit());
+
+            Console.WriteLine("kicik herifle baslayan yazi daxil edin:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine(str1.ToCapitalize());
+        }
+    }
+}
